05675564444b30bd9011f91d3f42b778545811cc
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-16 20:01:26 -0800

    basic panning based on head position

--- src/control.js
+++ src/control.js
@@ -41,3 +41,33 @@ function render()
     document.getElementById("leftPlane").setAttribute("position", positionL)
     document.getElementById("rightPlane").setAttribute("position", positionR)
 }
+
+const scene = document.querySelector('a-scene');
+if (scene.hasLoaded) {
+    subscribeToEvents();
+} else {
+    scene.addEventListener('loaded', subscribeToEvents);
+}
+
+function subscribeToEvents() {
+    const p = document.querySelector("#camera");
+    p.addEventListener('componentchanged', function (evt) {
+        //console.log(evt.detail.name);
+        if (evt.detail.name === 'rotation') {
+            //console.log('Rotation from ', evt.detail.oldData, 'to', evt.detail.newData, '!');
+            /*
+            panX = evt.detail.newData.x * 0.1;
+            panY = evt.detail.newData.y * 0.1;
+            panZ = evt.detail.newData.z * 0.1;
+            render();
+            */
+        }
+        if (evt.detail.name === 'position') {
+            //console.log('Movement from', evt.detail.oldData, 'to', evt.detail.newData, '!');
+            panX = evt.detail.newData.x * 1;
+            panY = evt.detail.newData.y * -1;
+            panZ = evt.detail.newData.z * 1;
+            render();
+        }
+    });
+}
