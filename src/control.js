35e64cb90fdd779e39a32262188dd742270fc80a
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-17 00:10:44 -0800

    refactors variables

--- src/control.js
+++ src/control.js
@@ -1,43 +1,38 @@
-// Base position of images
-var baseX = 0;
-var baseY = 0.1;
-var baseZ = -2.3;
-// Offset controlled by user's head
-var panX = 0;
-var panY = 0;
-var panZ = 0;
-// Per-eye offset for each image
-var deltaX = 0;
-var deltaY = 0;
-var deltaZ = 0;
-var step = 0.05;
+var positionBase = [0, 0.1, -2.3]; // Base position of images
+var positionOffset = [0, 0, 0]; // Offset controlled by user's head
+var positionOffsetFactor = [1, -1, 1]; // how user's head motion translates into offset
+var eyeDelta = [0, 0, 0]; // Per-eye offset for each image
+var eyeDeltaStep = 0.05;
+// TODO User defined origin for the camera position
 
 render();
 
 window.addEventListener("keydown", function(e){
     if(e.keyCode === 37) { // left
-        deltaX -= step;
+        eyeDelta[0] -= eyeDeltaStep;
         render();
     }
     if(e.keyCode === 39) { // right
-        deltaX += step;
+        eyeDelta[0] += eyeDeltaStep;
         render();
     }
     if(e.keyCode === 38) { // up
-        deltaY += step;
+        eyeDelta[1] += eyeDeltaStep;
         render();
     }
     if(e.keyCode === 40) { // down
-        deltaY -= step;
+        eyeDelta[1] -= eyeDeltaStep;
         render();
     }
 });
 
 function render()
 {
-    var positionR = (baseX + panX) + " " + (baseY + panY) + " " + (baseZ + panZ) + " ";
-    var positionL = (baseX + panX + deltaX) + " " + (baseY + panY + deltaY) + " " + (baseZ + panZ + deltaZ) + " ";
-    console.log(positionL + "; " + positionR);
+    var positionR = (positionBase[0] + positionOffset[0]) + " " + (positionBase[1] + positionOffset[1]) + " " + (positionBase[2] + positionOffset[2]);
+    var positionL = (positionBase[0] + positionOffset[0] + eyeDelta[0])
+             + " " + (positionBase[1] + positionOffset[1] + eyeDelta[1])
+             + " " + (positionBase[2] + positionOffset[2] + eyeDelta[2]);
+
     document.getElementById("leftPlane").setAttribute("position", positionL)
     document.getElementById("rightPlane").setAttribute("position", positionR)
 }
@@ -64,9 +59,9 @@ function subscribeToEvents() {
         }
         if (evt.detail.name === 'position') {
             //console.log('Movement from', evt.detail.oldData, 'to', evt.detail.newData, '!');
-            panX = evt.detail.newData.x * 1;
-            panY = evt.detail.newData.y * -1;
-            panZ = evt.detail.newData.z * 1;
+            positionOffset[0] = evt.detail.newData.x * positionOffsetFactor[0];
+            positionOffset[1] = evt.detail.newData.y * positionOffsetFactor[1];
+            positionOffset[2] = evt.detail.newData.z * positionOffsetFactor[2];
             render();
         }
     });
