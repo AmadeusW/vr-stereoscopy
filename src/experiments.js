d83f54cfb629968636f4cf4e84a5cfa9e200cc72
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-17 23:05:52 -0800

    places image on drawable canvas

--- src/experiments.js
+++ src/experiments.js
@@ -1,7 +1,3 @@
-var crop = false; // if false, split image in two. if true, use a cropping algorithm
-var dynamic = new Image();
-
-
 window.addEventListener("keydown", function(e){
     if(e.keyCode === 37) { // left
         dynamic.src = scenes[0].url;
@@ -11,23 +7,41 @@ window.addEventListener("keydown", function(e){
     }
 });
 
+function sizeup(x) {
+    var r = Math.pow(2, Math.ceil(Math.log2(x)));
+    console.log(x + " > " + r)
+    return r;
+}
+
+var dynamic = new Image();
 dynamic.src = 'http://orig00.deviantart.net/8afe/f/2012/092/1/4/parish_church_st__georg_3d_____cross_eye_hdr_by_zour-d4upy31.jpg';
 dynamic.onload = function() {
     console.log("loaded");
     var imageL = [0, 0, 0, 0]; // x, y, width, height
     var imageR = [0, 0, 0, 0]; // x, y, width, height
-    if (crop === false) {
+    if (1 == 1) { // todo: get selected scene's crop property
         imageL = [0, 0, dynamic.width/2, dynamic.height];
         imageR = [dynamic.width/2, 0, dynamic.width/2, dynamic.height];
     }
     else {
         console.log("not implemented");
     }
-    var canvasPosition = [0, 0, dynamic.width/2, dynamic.height]; // x, y, width, height
-    document.getElementById('canvasL').setAttribute("width", dynamic.width/2);
-    document.getElementById('canvasL').setAttribute("height", dynamic.height);
-    document.getElementById('canvasR').setAttribute("width", dynamic.width/2);
-    document.getElementById('canvasR').setAttribute("height", dynamic.height);
+    var canvasWidth = sizeup(dynamic.width/2);
+    var canvasHeight = sizeup(dynamic.height);
+    var deltaWidth = canvasWidth - dynamic.width / 2;
+    var deltaHeight = canvasHeight - dynamic.height;
+    var canvasPosition = [deltaWidth/2, deltaHeight/2, dynamic.width/2, dynamic.height]; // x, y, width, height
+
+    console.log(canvasWidth);
+    console.log(canvasHeight);
+    console.log(deltaWidth);
+    console.log(deltaHeight);
+    console.log(JSON.stringify(canvasPosition));
+
+    document.getElementById('canvasL').setAttribute("width", canvasWidth);
+    document.getElementById('canvasL').setAttribute("height", canvasHeight);
+    document.getElementById('canvasR').setAttribute("width", canvasWidth);
+    document.getElementById('canvasR').setAttribute("height", canvasHeight);
 
     document.getElementById('canvasL').getContext('2d').drawImage(
         dynamic, 
@@ -38,3 +52,4 @@ dynamic.onload = function() {
         imageR[0], imageR[1], imageR[2], imageR[3],
         canvasPosition[0], canvasPosition[1], canvasPosition[2], canvasPosition[3]);
 }
+
