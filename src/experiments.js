f693383e8c14df73496eaaf665c5c2bb509bbc75
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-17 19:37:54 -0800

    crops and draws two images!

--- src/experiments.js
+++ src/experiments.js
@@ -2,11 +2,11 @@ var dynamicL = new Image();
 dynamicL.src = 'images/l.jpg';
 dynamicL.onload = function() {
     console.log("loaded");
-    document.getElementById('canvasL').getContext('2d').drawImage(dynamicL, 0, 0, 256, 512);
+    document.getElementById('canvasL').getContext('2d').drawImage(dynamicL, 0, 0, 256, 512, 0, 0, 256, 512);
 }
 var dynamicL = new Image();
 dynamicL.src = 'images/r.jpg'; 
 dynamicL.onload = function() {
     console.log("loaded");
-    document.getElementById('canvasR').getContext('2d').drawImage(dynamicL, 0, 0, 256, 512);
+    document.getElementById('canvasR').getContext('2d').drawImage(dynamicL, 256, 0, 256, 512, 0, 0, 256, 512);
 }
