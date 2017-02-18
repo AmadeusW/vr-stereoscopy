5b5fbcba1f01ba7ad9423db07970b3da94c476b4
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-17 20:03:19 -0800

    vee arr

--- src/experiments.js
+++ src/experiments.js
@@ -2,6 +2,6 @@ var dynamic = new Image();
 dynamic.src = 'http://orig00.deviantart.net/8afe/f/2012/092/1/4/parish_church_st__georg_3d_____cross_eye_hdr_by_zour-d4upy31.jpg';
 dynamic.onload = function() {
     console.log("loaded");
-    document.getElementById('canvasL').getContext('2d').drawImage(dynamic, 0, 0, 1400, 1080, 0, 0, 256, 256);
-    document.getElementById('canvasR').getContext('2d').drawImage(dynamic, 0, 0, 1400, 1080, 0, 0, 256, 256);
+    document.getElementById('canvasL').getContext('2d').drawImage(dynamic, 0, 0, 700, 1080, 0, 0, 256, 256);
+    document.getElementById('canvasR').getContext('2d').drawImage(dynamic, 700, 0, 700, 1080, 0, 0, 256, 256);
 }
