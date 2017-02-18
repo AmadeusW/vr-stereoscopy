6aa6b2c1bde46b4be3b32fced6c8feae134cf5b0
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-17 19:46:34 -0800

    simplify

--- src/experiments.js
+++ src/experiments.js
@@ -1,12 +1,7 @@
-var dynamicL = new Image();
-dynamicL.src = 'http://orig00.deviantart.net/8afe/f/2012/092/1/4/parish_church_st__georg_3d_____cross_eye_hdr_by_zour-d4upy31.jpg';
-dynamicL.onload = function() {
+var dynamic = new Image();
+dynamic.src = 'http://orig00.deviantart.net/8afe/f/2012/092/1/4/parish_church_st__georg_3d_____cross_eye_hdr_by_zour-d4upy31.jpg';
+dynamic.onload = function() {
     console.log("loaded");
-    document.getElementById('canvasL').getContext('2d').drawImage(dynamicL, 0, 0, 256, 512, 0, 0, 256, 512);
-}
-var dynamicL = new Image();
-dynamicL.src = 'http://orig00.deviantart.net/8afe/f/2012/092/1/4/parish_church_st__georg_3d_____cross_eye_hdr_by_zour-d4upy31.jpg'; 
-dynamicL.onload = function() {
-    console.log("loaded");
-    document.getElementById('canvasR').getContext('2d').drawImage(dynamicL, 256, 0, 256, 512, 0, 0, 256, 512);
+    document.getElementById('canvasL').getContext('2d').drawImage(dynamic, 30, 30, 256, 512, 0, 0, 256, 512);
+    document.getElementById('canvasR').getContext('2d').drawImage(dynamic, 256, 0, 256, 512, 0, 0, 256, 512);
 }
