48da56ff74dec16dea39a8b94b1b73daca5ecba8
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-27 23:24:03 -0800

    cropping works :)

--- imageapp/Program.cs
+++ imageapp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using ImageSharp;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace imageapp
 {
@@ -41,6 +44,7 @@ namespace imageapp
 
         private static int findMargin(Image<Color> image, int searchX = 0, int searchY = 0)
         {
+            Debug.WriteLine($"findMargin: {searchX}, {searchY}");
             if (searchX == 0 && searchY == 0)
             {
                 throw new ArgumentException("Either searchX or searchY must be non-zero.");
@@ -50,52 +54,78 @@ namespace imageapp
                 throw new ArgumentException("Only one of searchX and searchY can be non-zero.");
             }
             // Find starting point
-            Point searchPoint;
+            Point[] searchPoints;
             if (searchX > 0)
             {
-                searchPoint = new Point(0, image.Height / 2);
+                searchPoints = new Point[] 
+                {
+                    new Point(0, image.Height / 4),
+                    new Point(0, image.Height / 4 * 2),
+                    new Point(0, image.Height / 4 * 3)
+                };
             }
             else if (searchX < 0)
             {
-                searchPoint = new Point(image.Width, image.Height / 2);
+                searchPoints = new Point[]
+                {
+                    new Point(image.Width - 1, image.Height / 4),
+                    new Point(image.Width - 1, image.Height / 4 * 2),
+                    new Point(image.Width - 1, image.Height / 4 * 3)
+                };
             }
             else if (searchY > 0)
             {
-                searchPoint = new Point(image.Width/ 2, 0);
+                searchPoints = new Point[]
+                {
+                    new Point (image.Width/ 4, 0),
+                    new Point (image.Width/ 4 * 2, 0),
+                    new Point (image.Width/ 4 * 3, 0)
+                };
             }
             else // if (searchY < 0)
             {
-                searchPoint = new Point(image.Width / 2, image.Height);
+                searchPoints = new Point[]
+                {
+                    new Point(image.Width / 4, image.Height - 1),
+                    new Point(image.Width / 4 * 2, image.Height - 1),
+                    new Point(image.Width / 4 * 3, image.Height - 1)
+                };
             }
 
             using (var pixels = image.Lock())
             {
-                Color lastColor = pixels[0, 0];
-
-                // Traverse into the picture. Stop when color differs
-                int verifyLength = searchY == 0 ? image.Height : image.Width; // If first pass was horizontal, traverse vertically
-                for (int i = 0; i < verifyLength; i++)
+                var distances = new List<int>();
+                foreach (var point in searchPoints)
                 {
-                    Console.WriteLine($"searching at {searchPoint}");
-                    var color = pixels[searchPoint.X, searchPoint.Y];
-                    if (!AlmostEquals(color, lastColor))
+                    var searchPoint = point;
+                    Color firstColor = pixels[point.X, point.Y];
+
+                    // Traverse into the picture. Stop when color differs
+                    int verifyLength = searchY == 0 ? image.Height : image.Width; // If first pass was horizontal, traverse vertically
+                    for (int i = 0; i < verifyLength; i++)
                     {
-                        Console.WriteLine($"First pass: stopping at {searchPoint}. Color was {color} but expected {lastColor}");
-                        return i;
+                        //Console.WriteLine($"searching at {searchPoint}");
+                        var color = pixels[searchPoint.X, searchPoint.Y];
+                        if (!AlmostEquals(color, firstColor))
+                        {
+                            Debug.WriteLine($"Candidate at {i}");
+                            distances.Add(i);
+                            break;
+                        }
+                        searchPoint.X += searchX;
+                        searchPoint.Y += searchY;
                     }
-                    lastColor = color;
-                    searchPoint.X += searchX;
-                    searchPoint.Y += searchY;
                 }
-                return 0; // in doubt, don't cut
+                Debug.WriteLine($"First pass: returning {distances.Min()}");
+                return distances.Min();
             }
         }
 
         public static bool AlmostEquals(Color a, Color b)
         {
-            if (a.R - b.R > 1 || a.R - b.R < -1) return false;
-            if (a.G - b.G > 1 || a.G - b.G < -1) return false;
-            if (a.B - b.B > 1 || a.B - b.B < -1) return false;
+            if (a.R - b.R > 9 || a.R - b.R < -9) return false;
+            if (a.G - b.G > 9 || a.G - b.G < -9) return false;
+            if (a.B - b.B > 9 || a.B - b.B < -9) return false;
             return true;
         }
     }
