04aade58f3f71d26d47698b7ec3f676d61a4f42d
Amadeusz Wieczorek <git@amadeusw.com>
2017-05-26 12:04:26 -0700

    Loads categories from external file

--- webui/menu.js
+++ webui/menu.js
@@ -13,11 +13,19 @@ function showMenu() {
     }
     
     document.querySelector("#menuTitle").setAttribute("value", "Welcome");
+    var template = document.querySelector("#categoryTemplate");
 
-    var template = document.querySelector("#category1");
-    var category = template.cloneNode(/*deep:*/true);
-    category.setAttribute("id", "category2");
-    var title = category.querySelector(".title");
-    title.setAttribute("value", "Second category");
-    menu.appendChild(category);
+    for (var categoryId = 0; categoryId < categories.length; categoryId++) {
+
+        var category = template.cloneNode(/*deep:*/true);
+        category.setAttribute("id", "category" + categoryId);
+        category.setAttribute("position", categoryId + " 0 0");
+        var title = category.querySelector(".title");
+        title.setAttribute("value", categories[categoryId].DisplayName);
+        category.querySelector(".thumbL").
+            setAttribute("src", "images/" + categories[categoryId].Thumbnail + ".T.L.jpg");
+        category.querySelector(".thumbR").
+            setAttribute("src", "images/" + categories[categoryId].Thumbnail + ".T.R.jpg");
+        menu.appendChild(category);
+    }
 }
