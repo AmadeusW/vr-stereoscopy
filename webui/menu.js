1769b54ed9af647bee5022aa4c9273aa97d5f9c2
Amadeusz Wieczorek <git@amadeusw.com>
2017-06-01 19:46:10 -0700

    attempt at invoking the animation programmatically

--- webui/menu.js
+++ webui/menu.js
@@ -13,6 +13,7 @@ function showMenu(categories) {
             .setAttribute("src", "images/" + categories[categoryId].Thumbnail + ".T.L.jpg");
         category.querySelector(".thumbR")
             .setAttribute("src", "images/" + categories[categoryId].Thumbnail + ".T.R.jpg");
+        category.querySelector(".categoryThumb").emit("grow");
         menu.appendChild(category);
 
         for (var subId = 0; subId < categories[categoryId].Subcategories.length; subId++) {
