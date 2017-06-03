a32071dbc08392dd6353109ef9ef95e05731932a
Amadeusz Wieczorek <git@amadeusw.com>
2017-06-02 17:44:20 -0700

    GOT THE EVENTS WORKING

--- webui/lib/aframe-listener.js
+++ webui/lib/aframe-listener.js
@@ -1,34 +1,26 @@
-  var select = function(obj) {
-    var data = obj.getAttribute('listener'); // because this.data doesn't work...
-    console.log('Select. My params: ', data.params);
-    obj.emit('grow');
+  var select = function(sender, params) {
+    sender.emit('grow');
   };
-  var deselect = function(obj) {
-    var data = obj.getAttribute('listener'); // because this.data doesn't work...
-    console.log('Deselect. My params: ', data.params);
-    obj.emit('shrink');
+  var deselect = function(sender, params) {
+    sender.emit('shrink');
   };
 
 AFRAME.registerComponent('listener', {
+  multiple: true,
   schema: {
     event: {type: 'string' },
     callback: {type: 'string', default: null},
     params: {type: 'array', default: []}
   },
   init: function () {
-    this.el.addEventListener('click', function (ev) {
-      /*
-      var fn = window[callback];
-if(typeof fn === 'function') {
-    fn(this, params);
-}*/
-      select(this);
-    });
-    this.el.addEventListener('mouseenter', function (ev) {
-      select(this);
-    });
-    this.el.addEventListener('mouseleave', function (ev) { /* todo: does it cover the case of fusing? */
-      deselect(this);
+    console.log('We are ', this.id);
+    var hack = this;
+    this.el.addEventListener(this.data.event, function (ev) {
+      var fn = window[hack.data.callback];
+      if(typeof fn === 'function') {
+        console.log('Calling function ', fn, ' on ', hack.el, ' with params ', hack.data.params);
+        fn(hack.el, hack.data.params);
+      }
     });
   },
 });
