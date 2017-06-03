9d6c717b5c483420e8ef1271c1b3323f8716fe44
Amadeusz Wieczorek <git@amadeusw.com>
2017-06-03 00:24:41 -0700

    html improvements

--- webui/menu.js
+++ webui/menu.js
@@ -60,7 +60,6 @@ function onDeselect(sender, params) {
 };
 
 function onConfirm(sender, params) {
-    console.log("confirm", params)
     if (params[0] == 'r') {
         nextImage();
     } else if (params[0] == 'l') {
