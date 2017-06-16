d5ba8ab9aef98b67f6441113036762cb36680941
Amadeusz Wieczorek <git@amadeusw.com>
2017-06-15 17:14:32 -0700

    adjusts the function that places category thumbnails

--- webui/menu.js
+++ webui/menu.js
@@ -36,7 +36,7 @@ function buildMenu(categories) {
         for (var subId = 0; subId < categories[categoryId].Subcategories.length; subId++) {
             var sub = template.cloneNode(/*deep:*/true);
             sub.setAttribute("id", "category" + categoryId + "sub" + subId);
-            placeInSpace(sub, (subId - 1) * 20)
+            placeInCircle(sub, (subId - 1) * 20)
             sub.setAttribute("visible", true);
             sub.querySelector(".title")
                 .setAttribute("value", categories[categoryId].Subcategories[subId].DisplayName);
@@ -52,12 +52,14 @@ function buildMenu(categories) {
     }
 }
 
-function placeInSpace(el, angle) {
+function placeInCircle(el, angle) {
   distance = 2
   x = distance * Math.sin(angle)
   y = distance * Math.cos(angle)
+  rotationY = -Math.acos(y/distance);
+  rotationX = -Math.acos(x/distance);
   el.setAttribute("position", x + " " + y + " 0")
-  el.setAttribute("rotation", "0 -" + angle + " 0")
+  el.setAttribute("rotation", rotationX + " " + rotationY + " 0")
 }
 
 function showMenu() {
