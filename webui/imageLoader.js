c395e7c8d91a5a18e453f8d57211c1fc56963340
Amadeusz Wieczorek <git@amadeusw.com>
2017-09-28 23:43:40 -0700

    blank the screen when changing pictures

--- webui/imageLoader.js
+++ webui/imageLoader.js
@@ -6,21 +6,37 @@ preloadImage = function(srcL, srcR) {
     tempImagesLoaded = 0;
     tempImageL.src = srcL;
     tempImageR.src = srcR;
-    document.getElementById("leftPlane").setAttribute("visibile", "false");
-    document.getElementById("rightPlane").setAttribute("visibile", "false");
+    document.getElementById("leftPlane").setAttribute("visible", "false");
+    document.getElementById("rightPlane").setAttribute("visible", "false");
 }
 
 onTempImageLoaded = function() {
     tempImagesLoaded++;
     if (tempImagesLoaded == 2) {
-        document.getElementById("leftPlane").setAttribute("visibile", "true");
-        document.getElementById("rightPlane").setAttribute("visibile", "true");
         render();
+        setTimeout(function() {
+            if (tempImagesLoaded == 2) { // 2 means reloadImage wasn't called during timeout
+                document.getElementById("leftPlane").setAttribute("visible", "true");
+                document.getElementById("rightPlane").setAttribute("visible", "true");
+            }
+        }, 100)
     }
+}
+
+onActualImageLoaded = function() {
+    console.log("onActualImageLoaded");
 }
 
 tempImageL = new Image();
 tempImageR = new Image();
+// NOTE: Ideally, I would use document.getElementById("leftPlane").materialtextureloaded
+// but this event doesn't seem to be fired, even if I update the material on render like so:
+// document.getElementById("leftPlane").setAttribute("material", "src: url(...)")
+// This means that instead of reacting to when image actually changes,
+// we get to react to when tempImage loads, hoping that web browser caches the image
+// and lets leftPlane load immediately (we give it small timeout to redraw)
 tempImageL.onload = onTempImageLoaded;
 tempImageR.onload = onTempImageLoaded;
+document.getElementById("leftPlane").materialtextureloaded  = onActualImageLoaded;
+document.getElementById("rightPlane").materialtextureloaded  = onActualImageLoaded;
 tempImagesLoaded = 0;
