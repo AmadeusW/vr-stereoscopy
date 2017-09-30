f5033edc048c06fafb7ea557b92fbf4bffdf2a66
Amadeusz Wieczorek <git@amadeusw.com>
2017-09-29 18:35:16 -0700

    adds flickrcrawler project

---
+++ crawler/FlickrCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlickrCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
